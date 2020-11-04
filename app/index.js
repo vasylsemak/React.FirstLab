48a3cdd78f5c2394648920d39f583252aa464850
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-03 21:29:54 -0500

    create increment method on Counter component

--- app/index.js
+++ app/index.js
@@ -3,6 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Counter extends React.Component {
+  constructor() {
+    super()
+    this.state = { count: 0 }
+    this.increment = this.increment.bind(this);
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 })
+  }
+
   render() {
     return (
       <div id="counter">
@@ -10,8 +20,8 @@ class Counter extends React.Component {
           Counter.js
         </div>
         <div id="counter">
-          <h1>0</h1>
-          <button>Increment</button>
+          <h1>{this.state.count}</h1>
+          <button onClick={this.increment}>Increment</button>
         </div>
       </div>
     )
