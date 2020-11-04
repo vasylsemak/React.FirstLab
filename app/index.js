28c44e130525ca0c286ed0043edb3ba61744bda3
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-03 21:07:51 -0500

    create Counter component in index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,21 @@
 // Your code here!
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div id="counter">
+        <div id="navbar">
+          Counter.js
+        </div>
+        <div id="counter">
+          <h1>0</h1>
+          <button>Increment</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Counter />, document.getElementById("app"));
